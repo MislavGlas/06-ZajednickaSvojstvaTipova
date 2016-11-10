a07c943a7f0144b5e978ac870e94cb25e1682d17
Mislav Glas <miglas@vsite.hr>
2016-11-10 19:44:51 +0100

    Ispravljen Equals() za Osobu

--- MetodaEqualsZaReferentiTip/MetodaEqualsZaReferentniTip.cs
+++ MetodaEqualsZaReferentiTip/MetodaEqualsZaReferentniTip.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace Vsite.CSharp
 {
-    // TODO: Definirati da klasa Osoba implementira sučelje IEquatable<Osoba>
+    // Definirati da klasa Osoba implementira sučelje IEquatable<Osoba>
     public class Osoba : IEquatable<Osoba>
     {
         public Osoba(string ime, int matičniBroj)
@@ -16,9 +16,10 @@ namespace Vsite.CSharp
         int m_matičniBroj;  // član vrijednosnog tipa
 
         // Implementirati metodu Equals(Osoba) iz sučelja IEquatable<Osoba> tako da za osobe s istim imenom i istim matičnim brojem rezultat bude true
-
         public bool Equals(Osoba other)
         {
+            if (other == null)
+                return false;
             if (m_matičniBroj != other.m_matičniBroj)
                 return false;
             return object.Equals(m_ime, other.m_ime);
